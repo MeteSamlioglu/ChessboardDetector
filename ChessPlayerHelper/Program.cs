88a4548eefaf9553eaf72b222e7b74dcaba2c312
Mete <60179489+MeteSamlioglu@users.noreply.github.com>
2023-11-27 06:05:01 +0300

    Dotnet console is added to test Numpy library in dotnet

---
+++ ChessPlayerHelper/Program.cs
@@ -0,0 +1,24 @@
+﻿/* 
+    A module that combines chess board detection modules in this files
+*/
+using NumSharp;
+
+class Program
+{
+    static void Main()
+    {
+        // Create a 1D array
+        var array1D = np.arange(0, 10);
+        Console.WriteLine("1D Array: " + array1D);
+
+        // Create a 2D array
+        var array2D = np.arange(0, 12).reshape(3, 4);
+        Console.WriteLine("2D Array:");
+        //Accessing elements
+        Console.WriteLine("Element at (1, 2): " + array2D[1, 2]);
+
+        //Perform operations
+        var sum = np.sum(array2D);
+        Console.WriteLine("Sum of array2D: " + sum);
+    }
+}
